f3b6474998218968ca4743a2108e7ac3a6fed0c4
jackw2050 <jack2050@gmail.com>
2017-03-16 12:47:27 -0500

    updating to martial

--- js/custom.js
+++ js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function () {
     //this code is for the gmap
     var map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
+        lat: 30.348088,
+        lng:  -97.714745
     });
 
 
